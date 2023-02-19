3684371ae415ec08f267f8036252d3f6039fb982
jeongyeonKimm <wjddus9132@gmail.com>
2023-02-19 22:06:25 +0900

    variables

--- main.dart
+++ main.dart
@@ -1,7 +1,25 @@
 void main() {
-  var name = '정연';
+  var name = '정연'; // String name = '정연';
   name = 'jeongyeon';
 
-  String name2 = '정연';
+  var name2; // dynamic name2;
   name2 = 'jeongyeon';
+  name2 = 12;
+  name2 = true;
+
+  String? jeongyeon = 'jeongyeon'; // null safety
+  jeongyeon = null;
+  if (jeongyeon != null) {
+    // jeongyeon?.isNotEmpty;
+    jeongyeon.isNotEmpty;
+  }
+
+  final name3 = 'jeongyeon'; // Final variable
+
+  late final String name4; // Late variable
+  // do something, go to api
+  name4 = '';
+  print(name4);
+
+  const API = '121212'; // 'Const' should be known at compile time
 }
